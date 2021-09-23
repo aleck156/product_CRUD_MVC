b52734c3a0720de5a38f620426235d23b3b855ff
Aleksander Jaworski <aleck156@gmail.com>
2021-09-23 11:27:11 +0200

    define routes for create, update and delete methods with get and post request

--- public/index.php
+++ public/index.php
@@ -8,8 +8,18 @@ use app\Router;
 use app\controllers\ProductController;
 
 $router = new Router();
+
 $router->get('/', [ProductController::class, 'index']);
-$router->get('/', [ProductController::class, 'index']);
-$router->get('/', [ProductController::class, 'index']);
+$router->get('/products', [ProductController::class, 'index']);
+
+$router->get('/products/create', [ProductController::class, 'create']);
+$router->post('/products/create', [ProductController::class, 'create']);
+
+$router->get('/products/update', [ProductController::class, 'update']);
+$router->post('/products/update', [ProductController::class, 'update']);
+
+$router->get('/products/delete', [ProductController::class, 'delete']);
+$router->post('/products/delete', [ProductController::class, 'delete']);
+
 $router->get('/', [ProductController::class, 'index']);
 ?>
