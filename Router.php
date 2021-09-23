c4c3aaedcdcbf989bc82c0546369e17fc0c31069
Aleksander Jaworski <aleck156@gmail.com>
2021-09-23 11:32:36 +0200

    define posstRoutes array, save url and function to call on that URL

--- Router.php
+++ Router.php
@@ -4,13 +4,14 @@ namespace app;
 
 class Router{
   public array $getRoutes = [];
+  public array $postRoutes = [];
 
   public function get($url, $fn){
-
+    $this->getRouter[$url] = $fn;
   }
 
   public function post($url, $fn){
-
+    $this->postRoutes[$url] = $fn;
   }
 
   public function resolve(){
