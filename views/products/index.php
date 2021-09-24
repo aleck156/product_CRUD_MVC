d4a7fb5c62e7c8aec90d5d2bade1631be80787d1
Aleksander Jaworski <aleck156@gmail.com>
2021-09-24 16:15:17 +0200

    copy code from CRUD index file

--- views/products/index.php
+++ views/products/index.php
@@ -1,7 +1,50 @@
 <h1>Product list</h1>
 
-<?php
-echo "<pre>";
-var_dump($products);
-echo "</pre>";
-?>
+<p>
+      <a href="create.php" class='btn btn-success'>Create New Product</a>
+    </p>
+
+    <form>
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Search for products ..." name="search"
+          value="<?php echo $search; ?>">
+        <button class="btn btn-outline-secondary" type="submit">
+          <i class="bi bi-search"></i>
+        </button>
+      </div>
+    </form>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">PID</th>
+          <th scope="col">Image</th>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Create Date</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach($products as $i => $product): ?>
+              <tr>
+                <th scope="row"><?php echo $i+1 ?></th>
+                <td><?php echo $product['id'] ?></td>
+                <td>
+                  <img src="../<?php echo $product['image'] ?>" alt="" class='thumb-image'>
+                </td>
+                <td><?php echo $product['title'] ?></td>
+                <td><?php echo $product['price'] ?></td>
+                <td><?php echo $product['create_date'] ?></td>
+                <td>
+                  <a href="update.php?id=<?php echo $product['id']?>" type="button" class="btn btn-sm btn-outline-primary">Edit</a>
+                  <form style="display:inline-block" action="delete.php" method="post">
+                    <input type="hidden" name="id" value="<?php echo $product['id']?>">
+                    <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                  </form>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+      </tbody>
+    </table>
