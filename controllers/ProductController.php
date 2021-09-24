401f785cf3dfae6a4a3eb6acece3b58598859c4d
Aleksander Jaworski <aleck156@gmail.com>
2021-09-24 14:24:39 +0200

    import Router class, add router to argument list, renderView based on passed argument

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace app\controllers;
+use app\Router;
 
 class ProductController {
-  public function index(){
-    echo "Index page";
+  public function index(Router $router){
+    return $router->renderView('products/index');
   }
 
-  public function create(){
-    echo "Create page";
+  public function create(Router $router){
+    return $router->renderView('products/create');
   }
 
   public function update(){
