458ba9c7af7832ae5117fb0a19e57f12f88e9bb0
Aleksander Jaworski <aleck156@gmail.com>
2021-09-24 11:06:40 +0200

    replace class definition with new instances of ProductController class

--- public/index.php
+++ public/index.php
@@ -1,22 +1,23 @@
 <?php
 
-require_once __DIR__."/../vendor/autoload.php";
 
-use app\Router;
 use app\controllers\ProductController;
+use app\Router;
 
+require_once __DIR__."/../vendor/autoload.php";
 $router = new Router();
 
-$router->get('/', [ProductController::class, 'index']);
-$router->get('/products', [ProductController::class, 'index']);
+$router->get('/', [new ProductController(), 'index']);
+$router->get('/products', [new ProductController(), 'index']);
+$router->get('/products/index', [new ProductController(), 'index']);
 
-$router->get('/products/create', [ProductController::class, 'create']);
-$router->post('/products/create', [ProductController::class, 'create']);
+$router->get('/products/create', [new ProductController(), 'create']);
+$router->post('/products/create', [new ProductController(), 'create']);
 
-$router->get('/products/update', [ProductController::class, 'update']);
-$router->post('/products/update', [ProductController::class, 'update']);
+$router->get('/products/update', [new ProductController(), 'update']);
+$router->post('/products/update', [new ProductController(), 'update']);
 
-$router->post('/products/delete', [ProductController::class, 'delete']);
+$router->post('/products/delete', [new ProductController(), 'delete']);
 
 $router->resolve();
 ?>
