e03a44c1a03951312381c4bfc2c23fb563fc863d
Aleksander Jaworski <aleck156@gmail.com>
2021-09-24 11:24:29 +0200

    replace local $pdo with $pdo tied to the instance of Database

--- Database.php
+++ Database.php
@@ -9,8 +9,8 @@ class Database {
 
   public function __construct()
   {
-    $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $this->pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
+    $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   }
 }
 
