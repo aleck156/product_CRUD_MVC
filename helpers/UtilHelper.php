5310cc1987ac056a0269b74eb942038b49cfd7b4
Aleksander Jaworski <aleck156@gmail.com>
2021-10-01 11:23:42 +0200

    make randomString function static

--- helpers/UtilHelper.php
+++ helpers/UtilHelper.php
@@ -4,7 +4,7 @@ namespace app\helpers;
 
 class UtilHelper {
 
-  public function randomString($n){
+  public static function randomString($n){
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     $str = '';
     for ($i = 0; $i < $n ; $i++){
