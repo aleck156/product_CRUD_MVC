9b40e4c7d4518316241b4218f012db76adc9096d
Aleksander Jaworski <aleck156@gmail.com>
2021-10-01 09:46:32 +0200

    define basic Product attributes

--- models/Product.php
+++ models/Product.php
@@ -2,7 +2,14 @@
 
 namespace app\models;
 
+// mapping of a class into a database product
 class Product {
+  public ?int $id = null;
+  public ?string $title = null;
+  public ?string $description = null;
+  public ?float $price = null;
+  public ?string $imagePath = null;
+  public ?array $imageFile = null;
   
 }
 
