3d0768d5613cc79c742830490de18625e9d73924
Aleksander Jaworski <aleck156@gmail.com>
2021-10-01 11:11:11 +0200

    singleton - only one instance of db connection, made static after initialization

--- Database.php
+++ Database.php
@@ -7,11 +7,13 @@ use app\models\Product;
 class Database {
 
   public \PDO $pdo;
+  public static Database $db;
 
   public function __construct()
   {
     $this->pdo = new PDO('mysql:host=127.0.0.1;port=3306;dbname=products_crud', 'root', '');
     $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    self::$db = $this;
   }
 
   public function getProducts($search = ''){
