4057caaa01417a2c95eb93cc7bcfde9ba3c4a81b
Aleksander Jaworski <aleck156@gmail.com>
2021-10-01 09:50:04 +0200

    define method load()

--- models/Product.php
+++ models/Product.php
@@ -10,6 +10,15 @@ class Product {
   public ?float $price = null;
   public ?string $imagePath = null;
   public ?array $imageFile = null;
+
+  public function load($data){
+    $this->id = $data['id'] ?? null;
+    $this->title = $data['title'];
+    $this->description = $data['description'] ?? '';
+    $this->price = $data['price'];
+    $this->imageFile = $data['imageFile'] ?? null;
+    $this->imagePath = $data['image'] ?? null;
+  }
   
 }
 
