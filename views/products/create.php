deebc737fb0720096c0cd6d6938531f3e1d94409
Aleksander Jaworski <aleck156@gmail.com>
2021-10-18 09:52:52 +0200

    change href address on both return buttons

--- views/products/create.php
+++ views/products/create.php
@@ -1,7 +1,7 @@
 <h1>Create product</h1>
 
 <p>
-  <a href="index.php" class="btn btn-secondary">Back to the main page</a>
+  <a href="/" class="btn btn-secondary">Back to the main page</a>
 </p>
 
 <?php include "_form.php" ?>
--- views/products/update.php
+++ views/products/update.php
@@ -1,7 +1,7 @@
 <h1>Update product</h1>
 
 <p>
-  <a href="index.php" class="btn btn-secondary">Back to the main page</a>
+  <a href="/" class="btn btn-secondary">Back to the main page</a>
 </p>
 
 <?php
