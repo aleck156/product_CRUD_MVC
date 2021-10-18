6f485a60440a329c9943fe43654e30a6dc2ca9a1
Aleksander Jaworski <aleck156@gmail.com>
2021-10-18 10:10:21 +0200

    get updated data from user via POST method

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -55,6 +55,21 @@ class ProductController {
     }
     $productData = $router->db->getProductById($id);
 
+    if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+      $productData['title'] = $_POST['title'];
+      $productData['description'] = $_POST['description'];
+      $productData['price'] = (float) $_POST['price'];
+      $productData['imageFile'] = $_FILES['image'] ?? null;
+
+      $product = new Product();
+      $product->load($productData);
+      $errors = $product->save();
+      if (empty($errors)){
+        header('Location: /products');
+        exit;
+      }
+    }
+
     return $router->renderView('products/update', [
       'product' => $productData
     ]);
