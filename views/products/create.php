c1478a84d81e11526dcda34539a02176a0ca569b
Aleksander Jaworski <aleck156@gmail.com>
2021-10-18 09:50:35 +0200

    add link to the main page on create and update pages

--- views/products/create.php
+++ views/products/create.php
@@ -1,3 +1,7 @@
 <h1>Create product</h1>
 
+<p>
+  <a href="index.php">Back to the main page</a>
+</p>
+
 <?php include "_form.php" ?>
--- views/products/update.php
+++ views/products/update.php
@@ -1,5 +1,9 @@
 <h1>Update product</h1>
 
+<p>
+  <a href="index.php">Back to the main page</a>
+</p>
+
 <?php
   include_once "_form.php"
 ?>
